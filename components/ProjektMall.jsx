1160d6f80061fc6419e204db3316deeb4bf2eefa
1Tolv2 <sofiaq1@gmail.com>
2022-09-14 22:38:37 +0200

    Fixed projektmall

--- components/ProjektMall.jsx
+++ components/ProjektMall.jsx
@@ -9,9 +9,10 @@ const ProjektMall = ({ blok }) => {
             <li key={index}>
               <h2>{item.title}</h2>
               {item.preamble !== "" && <h3>{item.preamble}</h3>}
-              <p>{item.content}<a href={item.link.url}>Länk här</a><br/></p>
-              {item.content2 !== "" && <p>{item.content2} {item.link2 && item.link2?.url !== "" && <a href={item.link2?.url}>Länk här</a>}</p>}
-              {item.content3 !== "" &&<p>{item.content3} {item.link3 && item.link3?.url !== ""  && <a href={item.link3?.url}>Länk här</a>}</p>}<br/>
+              <ul>
+              <li><p>{item.content}<a href={item.link.url}>{item.link.url}</a><br/></p></li>
+              {item.content2 !== "" && <li><p>{item.content2} {item.link2 && item.link2?.url !== "" && <a href={item.link2?.url}>{item.link2.url}</a>}</p></li>}
+              {item.content3 !== "" && <li><p>{item.content3} {item.link3 && item.link3?.url !== ""  && <a href={item.link3?.url}>{item.link3.url}</a>}</p></li>}<br/></ul>
             </li>
           );
         })}
