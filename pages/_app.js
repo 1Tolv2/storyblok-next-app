e3dedae86add9b67e7bd707b161bfcef98a939df
Sofia Johnsson <90647726+1Tolv2@users.noreply.github.com>
2022-09-16 10:25:01 +0200

    DEPLOY2022-09-16 (#5)
    * removed unused components
    
    * testing a change

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import "../styles/globals.css";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
-import Feature from "../components/Feature";
 import Grid from "../components/Grid";
 import Page from "../components/Page";
-import Teaser from "../components/Teaser";
-import RandomCatFact from "../components/RandomCatFact";
 import FeaturedPosts from "../components/FeaturedPosts";
 import PostSearch from "../components/PostSearch";
 import PostFeed from "../components/PostFeed";
@@ -15,14 +12,11 @@ import WeatherWidget from "../components/WeatherWidget";
 const components = {
   "post-feed": PostFeed,
   "post-search": PostSearch,
-  "random-cat-fact": RandomCatFact,
   "featured-posts": FeaturedPosts,
   "blog-post": BlogPost,
   "projekt-mall": ProjektMall,
   "weather-widget": WeatherWidget,
-  feature: Feature,
   grid: Grid,
-  teaser: Teaser,
   page: Page,
 };
 
